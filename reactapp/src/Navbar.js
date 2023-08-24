4ff37bd05229c10af3574c900710e1b8909658a3
Iamneo <techadmin@iamneo.ai>
2023-08-24 06:18:11 +0000

    Backup Commit

---
+++ reactapp/src/Navbar.js
@@ -0,0 +1,15 @@
+// Navbar.js
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/login">Login</a></li>
+        <li><a href="/signup">Signup</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ reactapp/src/Signup.js
@@ -0,0 +1,15 @@
+// Signup.js
+import React, { Component } from 'react';
+
+class Signup extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Signup Page</h2>
+        {/* Add your signup form here */}
+      </div>
+    );
+  }
+}
+
+export default Signup;
